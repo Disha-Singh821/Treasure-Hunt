ab496f1708f16bbba8c4c946e506095c75da8805
Disha-Singh821 <83941997+Disha-Singh821@users.noreply.github.com>
2021-06-06 15:40:18 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white");
+    textSize(15);
+    text("C U T N I F N O", 700, 150);
+    fill("lightblue");
+    text("Hint: Performs a particular Task !!",700,178);
+
+    fill("white");
+    textSize(15);
+    text("A T A D E S B A", 100, 310);
+    text("Hint: Stores all information !!", 100,330);
+
+
+}
